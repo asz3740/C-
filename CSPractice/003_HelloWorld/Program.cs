b5fb0c86f1ee5a4b9a05398073848deb94a8c0f5
asz3740 <49752744+asz3740@users.noreply.github.com>
2020-06-24 21:05:06 +0900

    출력, 자료형

---
+++ CSPractice/003_HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("static void Main(string[] args)");
+        }
+
+       /* static void Main()
+        {
+            Console.WriteLine("static void Main()");
+        }
+
+        static int Main()
+        {
+            Console.WriteLine("static int Main()");
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("static int Main(string[] args)");
+        }*/
+
+    }
+}
---
+++ CSPractice/005_Variable2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Variable2
+{
+    class Program 
+    {
+        static void Main(string[] args )
+        {
+            int x = 100;
+            int y = 200;
+            int z = 300;
+
+            Console.WriteLine("x " + x);
+            Console.WriteLine("y " + y);
+            Console.WriteLine("z " + z);
+            
+        }
+    }
+}
